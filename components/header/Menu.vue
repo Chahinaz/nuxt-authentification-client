72220ea95a43a7906220d5f968b6c43a55a1cc63
Chahinaz <chahinaz@dindane.com>
2018-08-07 15:39:05 +0200

    2 add header for homePage

--- components/header/Menu.vue
+++ components/header/Menu.vue
@@ -4,8 +4,6 @@
       <nuxt-link class="header item" to="/">Home</nuxt-link>
       <nuxt-link class="header item" to="/cv">Resume</nuxt-link>
       <nuxt-link class="header item" to="/blog">Blog</nuxt-link>
-      <nuxt-link class="header item" to="/about">About</nuxt-link>
-
       <nuxt-link class="header item" to="/" v-if="disconnected">Login</nuxt-link>
       <nuxt-link class="header item" to="/" v-if="!disconnected">Logout</nuxt-link>
   </div>
@@ -17,7 +15,7 @@
     props: {
       disconnected: {
         type: Boolean,
-        default: false
+        default: true
       }
     }
   }
@@ -26,8 +24,7 @@
 <style>
   .inverted {
     border-radius: 0 !important;
-    background-color: #424856 !important;
+    background-color: #333230 !important;
     margin: 0 !important;
   }
-
 </style>
